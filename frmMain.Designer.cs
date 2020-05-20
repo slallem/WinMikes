c01cde9d7aa34454edb527c5befd6ad8e518c30a
Stephane <stephane.lallemagne@gmail.com>
2020-05-20 16:07:43 +0200

    initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WinMikes
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnListMM = new System.Windows.Forms.Button();
+            this.lbEvents = new System.Windows.Forms.ListBox();
+            this.btClearEvents = new System.Windows.Forms.Button();
+            this.lvDevices = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDefault = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chState = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chInfo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Préiphériques d\'entrée";
+            // 
+            // btnListMM
+            // 
+            this.btnListMM.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnListMM.Location = new System.Drawing.Point(746, 27);
+            this.btnListMM.Name = "btnListMM";
+            this.btnListMM.Size = new System.Drawing.Size(109, 48);
+            this.btnListMM.TabIndex = 3;
+            this.btnListMM.Text = "Refresh";
+            this.btnListMM.UseVisualStyleBackColor = true;
+            this.btnListMM.Click += new System.EventHandler(this.btnListMM_Click);
+            // 
+            // lbEvents
+            // 
+            this.lbEvents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbEvents.FormattingEnabled = true;
+            this.lbEvents.Location = new System.Drawing.Point(15, 299);
+            this.lbEvents.Name = "lbEvents";
+            this.lbEvents.Size = new System.Drawing.Size(715, 199);
+            this.lbEvents.TabIndex = 6;
+            // 
+            // btClearEvents
+            // 
+            this.btClearEvents.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btClearEvents.Location = new System.Drawing.Point(746, 299);
+            this.btClearEvents.Name = "btClearEvents";
+            this.btClearEvents.Size = new System.Drawing.Size(109, 43);
+            this.btClearEvents.TabIndex = 7;
+            this.btClearEvents.Text = "Clear";
+            this.btClearEvents.UseVisualStyleBackColor = true;
+            this.btClearEvents.Click += new System.EventHandler(this.btClearEvents_Click);
+            // 
+            // lvDevices
+            // 
+            this.lvDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvDevices.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chDefault,
+            this.chState,
+            this.chInfo});
+            this.lvDevices.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvDevices.HideSelection = false;
+            this.lvDevices.Location = new System.Drawing.Point(15, 27);
+            this.lvDevices.MultiSelect = false;
+            this.lvDevices.Name = "lvDevices";
+            this.lvDevices.Size = new System.Drawing.Size(715, 266);
+            this.lvDevices.TabIndex = 11;
+            this.lvDevices.UseCompatibleStateImageBehavior = false;
+            this.lvDevices.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Device";
+            this.chName.Width = 280;
+            // 
+            // chDefault
+            // 
+            this.chDefault.Text = "Default";
+            this.chDefault.Width = 51;
+            // 
+            // chState
+            // 
+            this.chState.Text = "State";
+            this.chState.Width = 66;
+            // 
+            // chInfo
+            // 
+            this.chInfo.Text = "Information";
+            this.chInfo.Width = 177;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 504);
+            this.Controls.Add(this.lvDevices);
+            this.Controls.Add(this.btnListMM);
+            this.Controls.Add(this.btClearEvents);
+            this.Controls.Add(this.lbEvents);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "Win Mikes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnListMM;
+        private System.Windows.Forms.ListBox lbEvents;
+        private System.Windows.Forms.Button btClearEvents;
+        private System.Windows.Forms.ListView lvDevices;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chDefault;
+        private System.Windows.Forms.ColumnHeader chState;
+        private System.Windows.Forms.ColumnHeader chInfo;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,187 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WinMikes
+{
+    public partial class FrmMain : Form, IMMNotificationClient
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            //For USB devices and so on
+            HardwareNotification.RegisterUsbDeviceNotification(this.Handle);
+
+            //For MM EndPoints (not only USB)
+            var enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            enumerator.RegisterEndpointNotificationCallback(this);
+
+            //Initial fill lists
+            updateMMList();
+            updateUSBList();
+
+        }
+
+        private void btnListMM_Click(object sender, EventArgs e)
+        {
+            updateMMList();
+        } 
+
+        private void btnUSBRefresh_Click(object sender, EventArgs e)
+        {
+            updateUSBList();
+        }
+
+        public void updateMMList()
+        {
+            var audioDevices = GetMMDevices();
+            lvDevices.Items.Clear();
+            foreach (var device in audioDevices)
+            {
+                var lvi = new ListViewItem();
+                lvi.Text = device.FriendlyName;
+                lvi.SubItems.Add("-");
+                lvi.SubItems.Add(device.State.ToString());
+                lvi.SubItems.Add(device.ID);
+                lvDevices.Items.Add(lvi);
+            }
+        }
+
+        private void updateUSBList()
+        {
+        }
+
+        static List<MMDevice> GetMMDevices()
+        {
+            List<MMDevice> res = new List<MMDevice>();
+            MMDeviceEnumerator names = new MMDeviceEnumerator();
+            var devices = names.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+            foreach (var device in devices) {
+                res.Add(device);
+            }
+            return res;
+        }
+
+        static List<String> GetUSBSoundDevices()
+        {
+            List<String> devices = new List<String>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_SoundDevice"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add((string)device.GetPropertyValue("ProductName"));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+    
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == HardwareNotification.WmDevicechange)
+            {
+                switch ((int)m.WParam)
+                {
+                    case HardwareNotification.DbtDeviceremovecomplete:
+                        ReactToUSBChange("USB Device Removed");
+                        break;
+                    case HardwareNotification.DbtDevicearrival:
+                        ReactToUSBChange("USB Device Added");
+                        break;
+                }
+            }
+        }
+
+        public void ReactToUSBChange(string msg)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate { ReactToUSBChange(msg); });
+                return;
+            }
+            // Log event
+            lbEvents.Items.Add(msg);
+            // Update Devices List
+            updateUSBList();
+        }
+
+        public void ReactToHardwareChange(string msg) 
+        {
+            if (this.InvokeRequired) 
+            {
+                this.Invoke((MethodInvoker)delegate { ReactToHardwareChange(msg); });
+                return;
+            }
+            // Log event
+            lbEvents.Items.Add(msg);
+            // Update Devices List
+            updateMMList();
+        }
+
+        void IMMNotificationClient.OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            ReactToHardwareChange(String.Format("OnDeviceStateChanged --> Device Id: {0} ; Device State: {1}", deviceId, newState));
+        }
+        void IMMNotificationClient.OnDeviceAdded(string pwstrDeviceId) {
+            ReactToHardwareChange(String.Format("OnDeviceAdded --> Device Id: {0}", pwstrDeviceId));
+        }
+        void IMMNotificationClient.OnDeviceRemoved(string deviceId) {
+            ReactToHardwareChange(String.Format("OnDeviceRemoved --> Device Id: {0}", deviceId));
+        }
+        void IMMNotificationClient.OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId) {
+            ReactToHardwareChange(String.Format("OnDefaultDeviceChanged --> Default Device Id: {0}", defaultDeviceId));
+        }
+        void IMMNotificationClient.OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) {
+            // way too verbose -- ReactToHardwareChange(String.Format("OnPropertyValueChanged --> Device Id: {0} ; Property: {1}", pwstrDeviceId, key));
+        }
+
+        private void btClearEvents_Click(object sender, EventArgs e)
+        {
+            lbEvents.Items.Clear();
+        }
+
+    }
+
+    class USBDeviceInfo
+    {
+        public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+        {
+            this.DeviceID = deviceID;
+            this.PnpDeviceID = pnpDeviceID;
+            this.Description = description;
+        }
+        public string DeviceID { get; private set; }
+        public string PnpDeviceID { get; private set; }
+        public string Description { get; private set; }
+    }
+
+}
